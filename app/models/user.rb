200259843132b21fae9a168e06e9e0690cad66b3
yama0922 <73219643+yama0922@users.noreply.github.com>
2020-11-22 11:39:44 +0900

    修正４

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,14 @@ class User < ApplicationRecord
  with_options presence: true do
   validates :nickname
   validates :email
+  validates :birth_date
+end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
   validates :last_name
   validates :first_name
   validates :last_name_kana
   validates :first_name_kana
-  validates :birth_date
 end
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上かつ英数字をそれぞれ含めてください" }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上かつ英数字をそれぞれ含めてください" } do
+ end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,7 +37,13 @@ RSpec.describe User, type: :model do
     expect(@user).to be_valid
   end
 
-
+  it "passwordが5文字以下であれば登録できないこと" do
+    @user.password = "12345"
+    @user.password_confirmation = "12345"
+    @user.valid?
+    expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+  end
+  
   it "passwordとpassword_confirmationが不一致では登録できないこと" do
     @user.password = "123456"
     @user.password_confirmation = "1234567"
@@ -45,6 +51,12 @@ RSpec.describe User, type: :model do
     expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
   end
 
+  it "パスワードが英字１文字、数字１文字以上入っていないと進めない" do
+    user = build(:user,password: "aaaaaaa",password_confirmation: "aaaaaaa")
+    user.valid?
+    expect(user.password_complexity).to include("は英字と数字をそれぞれ1文字以上含める必要があります")
+  end
+  
   it "last_nameが空では登録できないこと" do
     @user.nic_name = nil
     @user.valid?
@@ -87,5 +99,51 @@ RSpec.describe User, type: :model do
     expect(@user.errors.full_messages).to include("Password Include both letters and numbers")
   end
 
- end
+  it "名字が全角で入力されていないと進めない(アルファベットver)" do
+    user = build(:user,family_name: "yamada")
+    user.valid?
+    expect(user.errors[:family_name]).to include("は全角で入力してください")
+  
+  it "名字が全角で入力されていないと進めない(半角ｶﾀｶﾅver)" do
+      user = build(:user,family_name: "ﾔﾏﾀﾞ")
+      user.valid?
+      expect(user.errors[:family_name]).to include("は全角で入力してください")
+    end
+    it "名前(カナ)が全角入力されていないと進めない(アルファベットver)" do
+      user = build(:user,given_name_kana: "tarou")
+      user.valid?
+      expect(user.errors[:given_name_kana]).to include("は全角カタカナで入力してください")
+    end
+     
+    it "名前(カナ)が全角入力されていないと進めない(半角ｶﾀｶﾅver)" do
+      user = build(:user,given_name_kana:"ﾀﾛｳ")
+      user.valid?
+      expect(user.errors[:given_name_kana]).to include("は全角カタカナで入力してください")
+    end
+  
+    it "名字（カナ）がカタカナで入力されていないと進めない（ひらがなver）" do
+      user = build(:user,family_name_kana: "やまだ")
+      user.valid?
+      expect(user.errors[:family_name_kana]).to include("は全角カタカナで入力してください")
+    end
+  
+    it "名字（カナ）がカタカナで入力されていないと進めない(漢字ver)" do
+      user = build(:user,family_name_kana: "山田")
+      user.valid?
+      expect(user.errors[:family_name_kana]).to include("は全角カタカナで入力してください")
+    end
+
+    it "名前(カナ)がカタカナで入力されていないと進めない(ひらがなver)" do
+      user = build(:user,given_name_kana: "たろう")
+      user.valid?
+      expect(user.errors[:given_name_kana]).to include("は全角カタカナで入力してください")
+    end
+  
+    it "名前(カナ)がカタカナで入力されていないと進めない(漢字ver)" do
+      user = build(:user,given_name_kana:"ﾀﾛｳ")
+      user.valid?
+      expect(user.errors[:given_name_kana]).to include("は全角カタカナで入力してください")
+    end
+  
+  end
 end
