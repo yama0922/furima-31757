9f178a66faa0df2c19cd016ae47cc15306668ca8
yama0922 <73219643+yama0922@users.noreply.github.com>
2020-11-30 18:03:14 +0900

    商品削除機能1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:create,:edit, :update]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new,:create,:edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.all.order("created_at DESC")
     # @items = Item
@@ -34,6 +34,12 @@ class ItemsController < ApplicationController
       render :edit   
     end
   end
+  
+  def destroy
+    if @item.destroy
+       redirect_to root_path
+    end
+  end   
   
   private
 
