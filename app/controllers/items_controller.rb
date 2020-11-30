7e6b94c9635aa6585ba58781f9aadfc86881bd3c
yama0922 <73219643+yama0922@users.noreply.github.com>
2020-11-30 19:00:12 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,10 @@ class ItemsController < ApplicationController
   end
   
   def destroy
-    if @item.destroy
-       redirect_to root_path
+    if current_user.id == @item.user.id && @item.destroy
+       redirect_to root_path 
+    else
+      render :show
     end
   end   
   
