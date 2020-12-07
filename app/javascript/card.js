b994f706870bf8c5622531575e6b315bbadd1991
yama0922 <73219643+yama0922@users.noreply.github.com>
2020-12-07 16:55:53 +0900

    修正3

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY1); 
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY); 
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
